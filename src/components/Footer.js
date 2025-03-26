8bdca9e9077276c44356c0c1bc0258d3493a8750
OwenC <66129237+Fxreball@users.noreply.github.com>
2025-03-26 16:55:34 +0100

    Copyright jaar aangepast.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './Footer.css'; // Zorg ervoor dat je deze CSS-bestand aanmaakt
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className='footer'>
-        <span>&copy; 2024 - Owen Coenraad</span>
+      <span>&copy; {currentYear} - Owen Coenraad</span>
     </footer>
   );
 };
